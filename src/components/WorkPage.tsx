4968ef28f666570c3e62d682adb583b9fcf1e636
Wenhao (Leo) Liang <36321327+foxhatleo@users.noreply.github.com>
2023-06-27 20:09:33 -0400

    Fix deployment issue

--- src/components/WorkPage.tsx
+++ src/components/WorkPage.tsx
@@ -19,6 +19,9 @@ import {faApple} from "@fortawesome/free-brands-svg-icons/faApple";
 import {faMicrosoft} from "@fortawesome/free-brands-svg-icons/faMicrosoft";
 
 function canUseWebP() {
+    if (typeof document === "undefined") {
+        return true;
+    }
     const elem = document.createElement("canvas");
     if (!!(elem.getContext && elem.getContext("2d"))) {
         return elem.toDataURL("image/webp").indexOf("data:image/webp") == 0;
